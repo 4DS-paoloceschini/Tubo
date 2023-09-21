4aacf3cb9f0bbb70be6c026d2c922fa183a5754e
4DS-paoloceschini <paolo.ceschini@buonarroti.tn.it>
2023-09-21 14:32:45 +0200

    feat: aggiunta del metodo costruttore

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,5 +1,10 @@
 public class Tubo {
     private float altezza, raggio;
-    private float pGreco;
+    private double pGreco;
 
+    public Tubo(float altezza, float raggio){
+        this.altezza=altezza;
+        this.raggio=raggio;
+        pGreco=3.14;
+    }
 }
